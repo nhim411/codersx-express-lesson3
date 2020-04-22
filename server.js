37e1369774f9248fa0f03e1b66127fd8f5cec973
Glitch (nodejs-4-post-method) <none>
2020-04-22 04:01:46 +0000

    💈🌥 Updated with Glitch

--- server.js
+++ server.js
@@ -3,35 +3,44 @@
 
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require('express');
+const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
-var todoLists = ['Đi chợ','Nấu cơm','Rửa bát','Học code tại CodersX'];
+var todoLists = ["Đi chợ", "Nấu cơm", "Rửa bát", "Học code tại CodersX"];
 
-app.set('views', './views');
-app.set('view engine', 'pug')
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 
 // https://expressjs.com/en/starter/basic-routing.html
-app.get('/', (req, res) => {
-  res.send('I love CodersX');
+app.get("/", (req, res) => {
+  res.render('index');
 });
 
-app.get('/todos', (req, res) => {
+app.get("/todos", (req, res) => {
   var q = req.query.q;
-  var query = (q == null) ? '' : q;
-  if(q !== null) {
-    var matchedLists = todoLists.filter(function(todo) {
-      return todo.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  if (!q) {
+    res.render("todos", {
+      todos: todoLists
     });
-    res.render('todos',{
-    todos: matchedLists
-  });
+  } else {
+    var matchedLists = todoLists.filter(function(todo) {
+      return todo.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    });
+    res.render("todos", {
+      todos: matchedLists
+    });
   }
-  else {
-    res.render('todos',{
-    todos: todoLists
-  });
-  }
+});
+
+app.post('/todos/create', function(req, res) {
+  var newTodo = req.body.todo;
+  todoLists.push(newTodo);
+  res.redirect('back');
 });
 
 // listen for requests :)
